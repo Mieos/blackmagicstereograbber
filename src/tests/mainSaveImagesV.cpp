1bc09880eb00a6d33ac108940f8cb4accbda68b0
Richard Modrzejewski <richard.modrzejewski@gmail.com>
2017-09-21 18:00:25 +0200

    Update saving tests

--- src/tests/mainSaveImagesV.cpp
+++ src/tests/mainSaveImagesV.cpp
@@ -5,6 +5,8 @@
 
 #include "VideoInputFromBlackMagic.hpp"
 
+#include <chrono>
+
 int main(int argc, char *argv[]){
 
    std::cout << "Test input" << std::endl;
@@ -25,7 +27,7 @@ int main(int argc, char *argv[]){
 
    bool endAsk=false;
 
-   std::string saveFolder = "imageSet2";
+   std::string saveFolder = "imageSetV";
    boost::filesystem::path dir(saveFolder.c_str());
    if(boost::filesystem::create_directory(dir)) {
       std::cout << "A new folder has been created for the pictures : " << saveFolder << "\n";
@@ -39,28 +41,46 @@ int main(int argc, char *argv[]){
    size_t numImage = 0;
    std::string imageNameR, imageNameL;
 
+
+   std::chrono::time_point<std::chrono::system_clock> start0, end0;
+   float elapsed_ms_0;
+
+
+   bool startRecord = false;
+
    while(!endAsk){
 
       //Get the current image
       testInput->getFrames(cImageL, cImageR);
 
+      if(startRecord){
+
+         end0 = std::chrono::system_clock::now();
+         elapsed_ms_0 = std::chrono::duration_cast<std::chrono::milliseconds>(end0-start0).count();
+
+         if(elapsed_ms_0>1000){
+            start0 = std::chrono::system_clock::now();
+            numImage++;
+            imageNameL= saveFolder + "/left/img" + std::to_string(numImage) + ".png";
+            imageNameR= saveFolder + "/right/img" + std::to_string(numImage) + ".png";
+            imwrite(imageNameL.c_str(), cImageL );
+            imwrite(imageNameR.c_str(), cImageR );
+            std::cout << "Images Saved : " << numImage << std::endl;
+
+         }
+
+      }
       //SHOW results !
       cv::imshow("Left", cImageL);
       cv::imshow("Right", cImageR);
 
       waitkeyNum = cv::waitKey(100);
-      
+
       if(waitkeyNum==1048603){
          endAsk=true;
       } else if(waitkeyNum==1048675){ //"c"
-         numImage++;
-         imageNameL= saveFolder + "/left/img" + std::to_string(numImage) + "left.png";
-         imageNameR= saveFolder + "/right/img" + std::to_string(numImage) + "right.png";
-         imwrite(imageNameL.c_str(), cImageL );
-         imwrite(imageNameR.c_str(), cImageR );
-         std::cout << "Images Saved : " << numImage << std::endl;
-         cv::imshow(imageNameL.c_str(), cImageL);
-         cv::imshow(imageNameR.c_str(), cImageR);
+         startRecord=true;
+         start0 = std::chrono::system_clock::now();
       }
    }
 
